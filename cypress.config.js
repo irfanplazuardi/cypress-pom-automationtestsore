49bf46210d8a4128b0238c240ee2360a26f0ace5
Irfan <irfanplazuardi@gmail.com>
2023-09-02 09:38:16 +0800

    setup cypress first commit

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
+    chromeWebSecurity: false,
+    baseUrl: "https://automationteststore.com/",
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 120000
+  },
+});
---
+++ cypress/e2e/register.js
@@ -0,0 +1,10 @@
+/// <reference types="Cypress" />
+
+describe('Register new account', () => {
+  beforeEach(() => {
+    cy.visit("/")
+  });
+  it('Invalid account register', () => {
+  cy.get
+  });
+});
